afef428df964e6d5c7363987b193a8e250865dbd
Kim Andre <Kim.Gronstol@student.uib.no>
2021-02-05 12:54:00 +0100

    Added basic grid Interface

---
+++ src/main/java/inf112/skeleton/app/grid/IGrid.java
@@ -0,0 +1,44 @@
+package inf112.skeleton.app.grid;
+
+/**
+ * IGrid is a generic gid system used for the backend and frontend
+ *
+ */
+public interface IGrid<T> {
+
+    /**
+     *
+     * Set the contents of the cell in the given x,y location.
+     *
+     * y must be 0 or higher but less or the same as the map height (getHeight()).
+     * x must be 0 or higher but less or the same as the map width (getWidth()).
+     *
+     * @param x The x coordinate of the cell to change the contents of.
+     * @param y The y coordinate of the cell to change the contents of.
+     * @param element The contents that should be set in the cell.
+     */
+    void set(int x, int y, T element);
+
+    /**
+     *
+     * Get the contents of the cell in the given x,y location.
+     *
+     * y must be 0 or higher but less or the same as the map height (getHeight()).
+     * x must be 0 or higher but less or the same as the map width (getWidth()).
+     *
+     * @param x The x coordinate of the cell to get the contents of.
+     * @param y The y coordinate of the cell to get the contents of.
+     */
+    T get(int x, int y);
+
+    /**
+     * @return The height of the grid.
+     */
+    int getHeight();
+
+    /**
+     * @return The width of the grid.
+     */
+    int getWidth();
+
+}
