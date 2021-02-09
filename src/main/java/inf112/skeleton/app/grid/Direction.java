53908ee7e75258bef0c359d69366ed799ead9e5c
Kim Andre <Kim.Gronstol@student.uib.no>
2021-02-09 15:41:28 +0100

    added directions

---
+++ src/main/java/inf112/skeleton/app/grid/Direction.java
@@ -0,0 +1,29 @@
+package inf112.skeleton.app.grid;
+
+public enum Direction {
+    NORTH(0, 0, -1), EAST(90, 1, 0 ), SOUTH(180, -1, 0), WEST(270, 0, 1);
+
+
+    private final double degrees;
+    private final int dx;
+    private final int dy;
+
+
+    Direction(double degrees, int dx, int dy) {
+        this.degrees = degrees;
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public double getDegrees() {
+        return degrees;
+    }
+
+    public int getDx() {
+        return dx;
+    }
+
+    public int getDy() {
+        return dy;
+    }
+}
