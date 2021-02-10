14b86944d497c6d9dbcf380fbba2d16d265d25db
BelminH <31646680+BelminH@users.noreply.github.com>
2021-02-10 14:48:58 +0100

    Forandret på implementasjon av hvordan man gir ut kortene

---
+++ src/main/java/inf112/skeleton/app/ICards.java
@@ -0,0 +1,12 @@
+package inf112.skeleton.app;
+
+public interface ICards {
+
+
+    /**
+     * give cards to player form cardstack
+     */
+    ICards getCards();
+
+    ICards shuffle(Stack<> c)
+}
