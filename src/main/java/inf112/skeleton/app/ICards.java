c377609f318a53b27f262dbdf3d9849ce92609fe
BelminH <31646680+BelminH@users.noreply.github.com>
2021-02-11 01:57:52 +0100

    added Icards

--- src/main/java/inf112/skeleton/app/ICards.java
+++ src/main/java/inf112/skeleton/app/ICards.java
@@ -7,7 +7,26 @@ public interface ICards {
      * give cards to player form cardstack
      * get the deck of cards
      */
+    ICards getDeck();
+
+    //shuffle the deck so its randomly ordered
+    ICards shuffle(Stack<> c);
+
+    //get 9 random cards from the deck
     ICards getCards();
 
-    ICards shuffle(Stack<> c)
+    //pick 5 cards of the 9 drawn
+    ICards pickCards();
+
+    //Choose the order to play your cards, then phase through the 5 phases for each card choosen
+    ICards cardOrder();
+
+
+    // After the 5 phases
+    ICards discardCards();
+
+    /**
+     * shuffles the discarded cards with the 'normal' cards
+     */
+    ICards reshuffle();
 }
