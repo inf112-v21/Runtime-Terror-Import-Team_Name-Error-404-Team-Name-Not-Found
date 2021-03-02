3b2f8f2ccd9393e2c2ba17baf53b1ce2f7e0dfdf
OdinBjerga <odin.bjerga@gmail.com>
2021-03-02 16:22:37 +0100

    Added card code

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -6,9 +6,9 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class Main {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
-        cfg.setTitle("hello-world");
-        cfg.setWindowedMode(480, 320);
+        cfg.setTitle("RoboRally");
+        cfg.setWindowedMode(1000, 1000);
 
-        new Lwjgl3Application(new HelloWorld(), cfg);
+        new Lwjgl3Application(new RoboRally(), cfg);
     }
 }
