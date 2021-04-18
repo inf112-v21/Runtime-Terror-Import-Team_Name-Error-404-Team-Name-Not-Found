6299eccc1830d2cbe0462715a28c2027cb3fc2c5
Kim Andre <Kim.Gronstol@student.uib.no>
2021-04-18 14:14:57 +0200

    finished implementing test for use of cards

--- src/test/java/inf112/skeleton/app/board/MoveOnCardTest.java
+++ src/test/java/inf112/skeleton/app/board/MoveOnCardTest.java
@@ -4,12 +4,11 @@ import inf112.skeleton.app.object.Belt;
 import inf112.skeleton.app.object.Card;
 import inf112.skeleton.app.object.CardType;
 import inf112.skeleton.app.object.Robot;
-import junit.framework.TestCase;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MoveOnCardTest extends TestCase {
+class MoveOnCardTest{
 
     private Board board;
     private Robot robot;
@@ -45,7 +44,7 @@ public class MoveOnCardTest extends TestCase {
     public void TestCardMoveForwardOne(){
         board = new Board(10,10);
         robot = new Robot(new Location(2,2, Direction.NORTH), board);
-        card = new Card(CardType.MOVE_FORWARDS_TWO, 200);
+        card = new Card(CardType.MOVE_FORWARDS_ONE, 200);
 
         Location expected = new Location(2,3,Direction.NORTH);
 
@@ -55,20 +54,55 @@ public class MoveOnCardTest extends TestCase {
     }
 
     @Test
-    public void TestCardMoveForwardOne(){
+    public void TestCardMoveBackwards(){
         board = new Board(10,10);
         robot = new Robot(new Location(2,2, Direction.NORTH), board);
-        card = new Card(CardType.MOVE_FORWARDS_TWO, 200);
+        card = new Card(CardType.MOVE_BACKWARDS, 200);
 
-        Location expected = new Location(2,3,Direction.NORTH);
+        Location expected = new Location(2,1,Direction.NORTH);
 
         robot.UseCards(card,robot);
 
         assertEquals(expected.getPosition(),robot.getPosition());
     }
 
+    @Test
+    public void TestCardRotateLeft(){
+        board = new Board(10,10);
+        robot = new Robot(new Location(2,2, Direction.NORTH), board);
+        card = new Card(CardType.ROTATE_LEFT, 200);
 
+        Location expected = new Location(2,2,Direction.WEST);
 
+        robot.UseCards(card,robot);
 
+        assertEquals(expected.getPosition(),robot.getPosition());
+    }
+
+    @Test
+    public void TestCardRotateRight(){
+        board = new Board(10,10);
+        robot = new Robot(new Location(2,2, Direction.NORTH), board);
+        card = new Card(CardType.ROTATE_RIGHT, 200);
+
+        Location expected = new Location(2,2,Direction.EAST);
+
+        robot.UseCards(card,robot);
+
+        assertEquals(expected.getPosition(),robot.getPosition());
+    }
+
+    @Test
+    public void TestCardTurnAround(){
+        board = new Board(10,10);
+        robot = new Robot(new Location(2,2, Direction.NORTH), board);
+        card = new Card(CardType.TURN_AROUND, 200);
+
+        Location expected = new Location(2,2,Direction.SOUTH);
+
+        robot.UseCards(card,robot);
+
+        assertEquals(expected.getPosition(),robot.getPosition());
+    }
 
 }
