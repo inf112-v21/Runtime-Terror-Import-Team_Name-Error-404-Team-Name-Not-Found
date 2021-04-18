cd4049fe0fec8bbc4a8063f281282be8f162f939
Kim Andre <Kim.Gronstol@student.uib.no>
2021-04-18 14:53:03 +0200

    changed input handler to use cards 1 = back 2 = one forward 3 = tow forward 4 = three forward 5 = rotate left 6 = rotate right 7 = turn around

--- src/main/java/inf112/skeleton/app/inputHandlers/GameScreenInputHandler.java
+++ src/main/java/inf112/skeleton/app/inputHandlers/GameScreenInputHandler.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import inf112.skeleton.app.RoboRally;
 import inf112.skeleton.app.board.Direction;
+import inf112.skeleton.app.object.CardType;
 import inf112.skeleton.app.screen.GameScreen;
 
 /** * a class that handles the input form keyboard when playing, making us able to play with the arrow keys
@@ -50,6 +51,41 @@ public class GameScreenInputHandler extends InputAdapter {
                 if (screen != null) {
                     screen.robot.walk(Direction.EAST);
                     game.render();
+                }
+                break;
+            case Input.Keys.NUM_1:
+                if (screen != null){
+                    screen.robot.UseCards(CardType.MOVE_BACKWARDS);
+                }
+                break;
+            case Input.Keys.NUM_2:
+                if (screen != null) {
+                    screen.robot.UseCards(CardType.MOVE_FORWARDS_ONE);
+                }
+                break;
+            case Input.Keys.NUM_3:
+                if (screen != null) {
+                    screen.robot.UseCards(CardType.MOVE_FORWARDS_TWO);
+                }
+                break;
+            case Input.Keys.NUM_4:
+                if (screen != null) {
+                    screen.robot.UseCards(CardType.MOVE_FORWARDS_THREE);
+                }
+                break;
+            case Input.Keys.NUM_5:
+                if (screen != null) {
+                    screen.robot.UseCards(CardType.ROTATE_LEFT);
+                }
+                break;
+            case Input.Keys.NUM_6:
+                if (screen != null) {
+                    screen.robot.UseCards(CardType.ROTATE_RIGHT);
+                }
+                break;
+            case Input.Keys.NUM_7:
+                if (screen != null) {
+                    screen.robot.UseCards(CardType.TURN_AROUND);
                 }
                 break;
             default:
